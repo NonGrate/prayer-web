22e82c5158138da77f3f73d93be12e1a4b296558
Arsenii Lisunov <arsenii.lis@gmail.com>
2017-05-19 18:26:26 +0200

    Add Need class, needs_list, AddPage, LikePage, ClearPage

--- main.py
+++ main.py
@@ -13,18 +13,20 @@
 # limitations under the License.
 
 import webapp2
-from webapp2_extras import json
+
+class Need:
+    id = 0
+    content = ""
+    rating = 0
+    color = 0
+
+
+needs_list = []
 
 
 class AllPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'application/json'
-        # response = {
-        #     'id': 123,
-        #     'content': "content",
-        #     'rating': 2,
-        #     'color': 16711935
-        # }
         self.response.out.write('[{"id": 123, "content": "content", "rating": 2, "color": 16711935}]')
 
 
@@ -34,9 +36,48 @@ class HelpPage(webapp2.RequestHandler):
         self.response.write('Help, I\'m inside this green robot!')
 
 
+class AddPage(webapp2.RequestHandler):
+    def post(self):
+        need = Need()
+        need.rating = 0
+        need.id = len(needs_list)
+        need.content = ""
+        need.color = ""
+
+        # needs_list.append(need)
+
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write(self.request)
+
+
+class LikePage(webapp2.RequestHandler):
+    def post(self):
+        need_id = self.response.get('id')
+        self.like_need(need_id)
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Help, I\'m inside this green robot!')
+
+    @staticmethod
+    def like_need(need_id):
+        for index, need in needs_list:
+            if need.id == need_id:
+                need.rating += 1
+                needs_list[index] = need
+                break
+
+
+class ClearPage(webapp2.RequestHandler):
+    def post(self):
+        global needs_list
+        needs_list = []
+        self.response.headers['Content-type'] = 'text/plain'
+        self.response.write(self.request)
+
+
 app = webapp2.WSGIApplication([
     ('/all', AllPage),
     ('/help', HelpPage),
-    # ('/add', AddPage),
-    # ('/like', LikePage)
+    ('/add', AddPage),
+    ('/like', LikePage)
+    ('/clear', ClearPage)
 ], debug=True)
