dc4b8c1f71da16562f3b4312134e5c5009684e75
Arsenii Lisunov <arsenii.lis@gmail.com>
2017-06-15 21:34:26 +0200

    virtual/bin/pip install -r requirements.txt

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/arseniii/Code/Workspaces/Prayer/Python/prayer-web-heroku/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
